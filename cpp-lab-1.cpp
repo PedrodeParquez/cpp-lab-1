65372a321ed304502791b87a9094f0bfa16ae9fd
Egor Mashin <egor.mashin.04@mail.ru>
2023-09-22 22:04:13 +0700

    Добавьте файлы проекта.

---
+++ cpp-lab-1.cpp
@@ -0,0 +1,185 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <stdio.h>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+class Athlete {
+public:
+  string lastName;
+  int birthYear;
+  double resultJump;
+
+  Athlete(string lastName, int birthYear, double jumpDistance);
+};
+
+Athlete::Athlete(string lastName, int birthYear, double resultJump)
+  : lastName(lastName),
+  birthYear(birthYear),
+  resultJump(resultJump) {
+
+}
+
+vector<Athlete> readTextFile(const string& filename);
+
+void mathematics();
+
+void athletesRank();
+
+
+int main() {
+  int numberTask;
+  setlocale(LC_ALL, "Russian");
+
+  cout << "Введите номер упражнения: ";
+  cin >> numberTask;
+
+  switch (numberTask) {
+  case 1:
+    mathematics();
+    break;
+  case 2:
+    athletesRank();
+    break;
+  default:
+    cout << "Некорректный ввод! Повтороно введите номер задачи.";
+    main();
+  }
+}
+
+void mathematics() {
+  double a, b, x1, x2, y1, y2;
+
+  cout << "Пожалуйста, введите x1: ";
+  cin >> x1;
+
+  while (cin.fail()) {
+    cin.clear(); 
+    cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
+    cout << "Некорректный ввод! Повторите попытку заново!\n"
+         << "Пожалуйста, введите x1 : ";
+    cin >> x1;
+  }
+
+  cout << "Пожалуйста, введите y1: ";
+  cin >> y1;
+
+  while (cin.fail()) {
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cout << "Некорректный ввод! Повторите попытку заново!\n"
+         << "Пожалуйста, введите y1 : ";
+    cin >> y1;
+  }
+
+  cout << "Пожалуйста, введите x2: ";
+  cin >> x2;
+
+  while (cin.fail()) {
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cout << "Некорректный ввод! Повторите попытку заново!\n"
+         << "Пожалуйста, введите x2: ";
+    cin >> x2;
+  }
+
+  cout << "Пожалуйста, введите коэффициент a: ";
+  cin >> a;
+
+  while (cin.fail()) {
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cout << "Некорректный ввод! Повторите попытку заново!\n"
+         << "Пожалуйста, введите a: ";
+    cin >> a;
+  }
+
+  cout << "Пожалуйста, введите коэффициент b: ";
+  cin >> b;
+
+  while (cin.fail()) {
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cout << "Некорректный ввод! Повторите попытку заново!\n"
+         << "Пожалуйста, введите b: ";
+    cin >> b;
+  }
+
+  y2 = a * x2 + b;
+
+  cout << "Ордината точки O2 равна: " << fixed << setprecision(2) << y2;
+ 
+}
+
+void athletesRank() {
+  int searchYear;
+  vector<Athlete> matchingAthletes;
+
+  string filename = "AthletesData.txt";
+
+  vector<Athlete> athletes = readTextFile(filename);
+
+  cout << "Введите год рождения для поиска: ";
+  cin >> searchYear;
+
+  while (cin.fail()) {
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cout << "Некорректный ввод! Повторите попытку заново!\n"
+         << "Пожалуйста, введите год рождения для поиска : ";
+    cin >> searchYear;
+  }
+
+  for (const Athlete& athlete : athletes) {
+    if (athlete.birthYear == searchYear) {
+      matchingAthletes.push_back(athlete);
+    }
+  }
+
+  if (!matchingAthletes.empty()) {
+    auto bestAthlete = max_element(matchingAthletes.begin(), matchingAthletes.end(),
+      [](const Athlete& a, const Athlete& b) {
+        return a.resultJump < b.resultJump;
+      });
+
+    cout << "Спортсмен, родившийся в " << searchYear << " году, с лучшим результатом:\n";
+    cout << "Фамилия: " << bestAthlete->lastName << "\n";
+    cout << "Результат: " << bestAthlete->resultJump << " метра\n";
+  }
+  else {
+    cout << "Спортсменов, родившихся в " << searchYear << " году, не найдено.\n";
+  }
+}
+
+vector <Athlete> readTextFile(const string& filename) {
+  vector <Athlete> athletes;
+  ifstream file(filename);
+
+  if (!file.is_open()) {
+    cerr << "Ошибка при открытии файла для чтения" << endl;
+    return athletes;
+  }
+
+  string line;
+
+  while (getline(file, line)) {
+    istringstream iss(line);
+    std::string lastName;
+    int birthYear;
+    double resultJump;
+
+    if (iss >> lastName >> birthYear >> resultJump) {
+      Athlete athlete(lastName, birthYear, resultJump);
+      athletes.push_back(athlete);
+    }
+  }
+
+  file.close();
+
+  return athletes;
+}
+

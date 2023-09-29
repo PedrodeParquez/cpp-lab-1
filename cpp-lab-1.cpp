e41b005744c900e80edbd169cecf0776d2f30d16
Egor Mashin <egor.mashin.04@mail.ru>
2023-09-30 03:05:06 +0700

    add some additional funcs and var test

--- cpp-lab-1.cpp
+++ cpp-lab-1.cpp
@@ -30,6 +30,14 @@ void mathematics();
 
 void athletesRank();
 
+void athletesChoice();
+
+void athletesAddToFile();
+
+bool isAlphaString(const string& str);
+
+void appendAthleteToFile(const string& filename, const Athlete& athlete);
+
 
 int main() {
   int numberTask;
@@ -43,7 +51,7 @@ int main() {
     mathematics();
     break;
   case 2:
-    athletesRank();
+    athletesChoice();
     break;
   default:
     cout << "Некорректный ввод! Повтороно введите номер задачи.";
@@ -86,7 +94,7 @@ void mathematics() {
          << "Пожалуйста, введите b: ";
     cin >> b;
   }
-
+  
   y2 = a * x2 + b;
 
   cout << "Ордината точки O2 равна: " << fixed << setprecision(2) << y2;
@@ -130,7 +138,98 @@ void athletesRank() {
   }
   else {
     cout << "Спортсменов, родившихся в " << searchYear << " году, не найдено.\n";
+    athletesRank();
   }
+}
+
+void athletesChoice() {
+  int choice;
+
+  cout << "Если вы хотите найти лучший результат - 1\n"
+       << "Если хотите добавить нового ученика - 2\n"\
+       << "Ваш выбор: ";
+  cin >> choice;
+
+  switch (choice) {
+    case 1:
+      athletesRank();
+      break;
+    case 2:
+      athletesAddToFile();
+      break;
+    default:
+      cout << "Некорректный ввод! Повтороно введите номер задачи.";
+      athletesChoice();
+  }
+}
+
+void athletesAddToFile() {
+  string lastName;
+  int birthYear;
+  double resultJump;
+
+  cout << "Введите фамилию спортсмена: ";
+  cin >> lastName;  
+
+  while (!isAlphaString(lastName)) {
+    cout << "Фамилия должна содержать только буквы. Пожалуйста, введите фамилию спортсмена: ";
+    cin >> lastName;
+  }
+
+  cout << "Введите год рождения спортсмена: ";
+  cin >> birthYear;
+
+  if (birthYear <= 0) {
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cout << "Некорректный ввод! Повторите попытку заново!\n"
+         << "Пожалуйста, введите год рождения спортсмена: ";
+    cin >> birthYear;
+  }
+
+  while (cin.fail()) {
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cout << "Некорректный ввод! Повторите попытку заново!\n"
+         << "Пожалуйста, введите год рождения спортсмена: ";
+    cin >> birthYear;
+  }
+
+  cout << "Введите результат прыжка спортсмена: ";
+  cin >> resultJump;
+
+  while (cin.fail()) {
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cout << "Некорректный ввод! Повторите попытку заново!\n"
+      << "Пожалуйста, введите результат прыжка спортсмена: ";
+    cin >> resultJump;
+  }
+
+  Athlete newAthlete(lastName, birthYear, resultJump);
+  appendAthleteToFile("AthletesData.txt", newAthlete);
+  cout << "Спортсмен успешно добавлен в файл." << endl;
+}
+
+bool isAlphaString(const string& str) {
+  for (char c : str) {
+    if (!isalpha(c)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void appendAthleteToFile(const string& filename, const Athlete& athlete) {
+  ofstream file(filename, ios::app);
+
+  if (!file.is_open()) {
+    cerr << "Ошибка при открытии файла для записи" << endl;
+    return;
+  }
+
+  file << athlete.lastName << " " << athlete.birthYear << " " << athlete.resultJump << endl;
+  file.close();
 }
 
 vector <Athlete> readTextFile(const string& filename) {
@@ -161,3 +260,4 @@ vector <Athlete> readTextFile(const string& filename) {
   return athletes;
 }
 
+//Переделать условие для первой задачи
